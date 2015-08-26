fd96b774caa5c382aac3671bcc9a245f153a542d
Jacek Kubiak <jacek.kubiak@netguru.pl>
2015-08-26 16:57:44 +0200

    Add engine source

---
+++ lib/cc/engine/flay.rb
@@ -0,0 +1,57 @@
+require 'flay'
+require 'json'
+
+module CC
+  module Engine
+    class Flay
+      def initialize(directory: ,engine_config: ,io:)
+        @directory = directory
+        @engine_config = engine_config
+        @io = io
+      end
+
+      def run
+        files = ::Flay.filter_files(::Flay.expand_dirs_to_files(directory))
+        flay.process(*files)
+
+        flay.report(StringIO.new).each do |issue|
+          puts "#{new_violation(issue).to_json}\n"
+        end
+      end
+
+      private
+
+      attr_reader :directory, :engine_config, :io
+
+      def flay
+        @flay ||= ::Flay.new
+      end
+
+      def new_violation(issue)
+        {
+          "type": "issue",
+          "check_name": name(issue),
+          "description": "Duplication found in #{issue.name}",
+          "categories": ["Duplication"],
+          "location": locations(issue)
+        }
+      end
+
+      def name(issue)
+        issue.identical? ? 'Identical code' : 'Similar code'
+      end
+
+      def locations(issue)
+        issue.locations.map do |loc|
+          {
+            "path": loc.file,
+            "lines": {
+              "begin": loc.line,
+              "end": loc.line
+            }
+          }
+        end
+      end
+    end
+  end
+end
