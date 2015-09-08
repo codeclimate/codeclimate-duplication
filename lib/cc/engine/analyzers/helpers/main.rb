dccee550480f333df12a1a5087081c5d795c5c3d
Jacek Kubiak <jacek.kubiak@netguru.pl>
2015-09-08 15:17:12 +0200

    Set up a ruby engine

---
+++ lib/cc/engine/analyzers/helpers/main.rb
@@ -0,0 +1,71 @@
+module CC
+  module Engine
+    module Analyzers
+      module Helpers
+        def flay
+          @flay ||= ::Flay.new(options)
+        end
+
+        def new_violation(issue, location, other)
+          {
+            "type": "issue",
+            "check_name": name(issue),
+            "description": "Duplication found in #{issue.name}",
+            "categories": ["Duplication"],
+            "location": format_location(location),
+            "other_locations": format_locations(other)
+          }
+        end
+
+        def name(issue)
+          issue.identical? ? 'Identical code' : 'Similar code'
+        end
+
+        def find_other_locations(all_locations, current)
+          all_locations.reject { |location| location == current }
+        end
+
+        def format_location(location)
+          {
+            "path": local_path(location.file),
+            "lines": {
+              "begin": location.line,
+              "end": location.line
+            }
+          }
+        end
+
+        def format_locations(other)
+          other.map { |location| format_location(location) }
+        end
+
+        def options
+          {
+            diff: false,
+            mass: mass_threshold,
+            summary: false,
+            verbose: false,
+            number: true,
+            timeout: 10,
+            liberal: false,
+            fuzzy: false,
+            only: nil
+          }
+        end
+
+        def local_path(file)
+          file.gsub(%r{^#{directory_path}/}, "")
+        end
+
+        def directory_path
+          @directory_path ||= Pathname.new(@directory).realpath.to_s
+        end
+
+        def excluded_files
+          return [] if engine_config["exclude_paths"].nil?
+          engine_config["exclude_paths"].map { |path| Dir.glob("#{directory}/path") }.flatten
+        end
+      end
+    end
+  end
+end
---
+++ lib/cc/engine/analyzers/ruby/main.rb
@@ -0,0 +1,54 @@
+require 'flay'
+require 'json'
+require 'cc/engine/analyzers/helpers/main'
+require 'pry'
+
+module CC
+  module Engine
+    module Analyzers
+      module Ruby
+        class Main
+          include ::CC::Engine::Analyzers::Helpers
+
+          def initialize(directory:, engine_config:, io:)
+            @directory = directory
+            @engine_config = engine_config || {}
+            @io = io
+          end
+
+          def run
+            flay.process(*analyzed_files)
+
+            flay.report(StringIO.new).each do |issue|
+              all_locations = issue.locations
+
+              all_locations.each do |location|
+                other_locations = find_other_locations(all_locations, location)
+                io.puts "#{new_violation(issue, location, other_locations).to_json}\0"
+              end
+            end
+          end
+
+          private
+
+          attr_reader :directory, :engine_config, :io
+
+          def filter_files(files)
+            return files if engine_config["exclude_paths"].nil?
+            files - excluded_files
+          end
+
+          def mass_threshold
+            mass = engine_config.fetch('config', {}).fetch('ruby', {}).fetch('mass_threshold', {})
+            mass.empty? ? 10 : mass
+          end
+
+          def analyzed_files
+            filter_files(::Flay.expand_dirs_to_files(directory))
+          end
+        end
+      end
+    end
+  end
+end
+
