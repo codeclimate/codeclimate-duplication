da386846184565ea584d48fa099732b22cf06f1b
patrick brisbin <pbrisbin@gmail.com>
2016-03-22 12:33:36 -0400

    Adjust file matching and expansion
    The include_paths may or may not be ./-prefixed (they usually aren't,
    but there's the notable case of ["./"]). Patterns are never ./-prefixed
    (they can't be for the purposes of relative expansion). Given this
    uncertainty, it's possible to drop files if you're not careful in how
    you match or expand. In addition to this, fingerprints in the wild are
    all computed with ./-prefixed paths. For all these reasons, we need to
    normalize the prefixing of paths and patterns in FileList.

--- lib/cc/engine/analyzers/file_list.rb
+++ lib/cc/engine/analyzers/file_list.rb
@@ -26,15 +26,24 @@ module CC
         attr_reader :engine_config, :patterns
 
         def expand(path)
-          globs = patterns.map { |p| File.join(path, p) }
+          globs = patterns.map { |p| File.join(relativize(path), p) }
 
           Dir.glob(globs)
         end
 
         def matches?(path)
           patterns.any? do |p|
-            File.fnmatch?(File.join("./", p), path, File::FNM_PATHNAME)
+            File.fnmatch?(
+              relativize(p),
+              relativize(path),
+              File::FNM_PATHNAME | File::FNM_EXTGLOB
+            )
           end
+        end
+
+        # Ensure all paths (and patterns) are ./-prefixed
+        def relativize(path)
+          "./#{path.sub(%r{^\./}, "")}"
         end
       end
     end
