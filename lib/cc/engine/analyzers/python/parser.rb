7abbf56debd0a77e4e897b0b0a1055cbf58e79ef
Will Fleming <will@flemi.ng>
2016-10-07 11:02:15 -0400

    Support python3 (#135)
    * install python3 in dockerfile
    
    * Refactor: make analyzer available to parser, break up some methods
    
    * Support python3 from config
    
    * Support a `python_version` value under `python` key in config
    * Use `python3` when this is set appropriately
    * Update the `parser.py` script to work in both versions
    
    * extract default version to constant

--- lib/cc/engine/analyzers/python/parser.rb
+++ lib/cc/engine/analyzers/python/parser.rb
@@ -9,7 +9,8 @@ module CC
         class Parser < ParserBase
           attr_reader :code, :filename, :syntax_tree
 
-          def initialize(code, filename)
+          def initialize(python_version, code, filename)
+            @python_version = python_version
             @code = code
             @filename = filename
           end
@@ -23,9 +24,24 @@ module CC
             self
           end
 
+          private
+
+          attr_reader :python_version
+
           def python_command
-            file = File.expand_path(File.dirname(__FILE__)) + '/parser.py'
-            "python #{file}"
+            file = File.expand_path(File.dirname(__FILE__)) + "/parser.py"
+            "#{python_binary} #{file}"
+          end
+
+          def python_binary
+            case python_version
+            when 2, "2"
+              "python2"
+            when 3, "3"
+              "python3"
+            else
+              raise ArgumentError, "Supported python versions are 2 and 3. You configured: #{python_version.inspect}"
+            end
           end
         end
       end
