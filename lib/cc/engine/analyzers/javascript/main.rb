fd923978bfaab4906a98ab69c15c1fd44a317906
Max Jacobson <max@hardscrabble.net>
2016-10-10 17:38:40 -0400

    Avoid OOM errors
    We're primarily seeing these on minified javascript files, so let's skip
    those.
    
    Also, in case there are other occurrences, let's also print the
    filenames when we process a file, to help get more information.
    Print filename before analyzing
    
    The skipping algorithm is mostly borrowed from https://github.com/codeclimate/codeclimate-eslint/pull/115
    
    Needed to change a fixture to make it less than 100 chars long, because
    it was being skipped
    
    Note: overriding the $stderr doesn't seem to interfere with tests that
    care about stderr, thankfully!

--- lib/cc/engine/analyzers/javascript/main.rb
+++ lib/cc/engine/analyzers/javascript/main.rb
@@ -1,5 +1,6 @@
 require "cc/engine/analyzers/analyzer_base"
 require "cc/engine/analyzers/javascript/parser"
+require "cc/engine/analyzers/javascript/minification_checker"
 require "cc/engine/analyzers/javascript/node"
 require "cc/engine/analyzers/file_list"
 require "flay"
@@ -26,6 +27,12 @@ module CC
 
           def js_parser
             ::CC::Engine::Analyzers::Javascript::Parser
+          end
+
+          def skip?(path)
+            if MinificationChecker.new(path).minified?
+              "the file is minified"
+            end
           end
         end
       end
