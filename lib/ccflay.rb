dd7866a78b409b2d7c29a3c2e5f6475948abe882
Ryan Davis <ryand-ruby@zenspider.com>
2017-05-22 15:31:57 -0700

    Update flay and remove code pushed upstream.
    This code also adds a bunch of node names to Sexp::NODE_NAMES for JS
    and PHP. It is probably not exhaustive.

--- lib/ccflay.rb
+++ lib/ccflay.rb
@@ -18,71 +18,54 @@ class CCFlay < Flay
     self.identical = Concurrent::Hash.new
     self.masses = Concurrent::Hash.new
   end
-
-  ##
-  # Calculate the structural hash for this sexp. Cached, so don't
-  # modify the sexp afterwards and expect it to be correct.
-
-  if ENV["PURE_HASH"]
-    def structural_hash
-      @structural_hash ||= pure_ruby_hash
-    end
-  else
-    def structural_hash
-      @structural_hash ||= Digest::MD5.hexdigest(structure.to_s)
-    end
-  end
 end
 
-# TODO: move this to flay proper... it's benchmarking faster
-class Sexp # straight from flay-persistent
-  names = %w[alias and arglist args array attrasgn attrset back_ref
-             begin block block_pass break call case cdecl class colon2
-             colon3 const cvar cvasgn cvdecl defined defn defs dot2
-             dot3 dregx dregx_once dstr dsym dxstr ensure evstr false
-             flip2 flip3 for gasgn gvar hash iasgn if iter ivar lasgn
-             lit lvar masgn match match2 match3 module next nil not
-             nth_ref op_asgn op_asgn1 op_asgn2 op_asgn_and op_asgn_or or
-             postexe redo resbody rescue retry return sclass self
-             splat str super svalue to_ary true undef until valias
-             when while xstr yield zsuper kwarg kwsplat safe_call]
+new_nodes = [
+             :And, :ArrayExpression, :ArrowFunctionExpression,
+             :Assign, :AssignmentExpression, :AssignmentPattern,
+             :Attribute, :BinaryExpression, :BlockStatement, :BoolOp,
+             :BooleanLiteral, :Break, :BreakStatement, :Call,
+             :CallExpression, :CatchClause, :ClassBody,
+             :ClassDeclaration, :ClassMethod, :Compare,
+             :ConditionalExpression, :Continue, :ContinueStatement,
+             :Dict, :Directive, :DirectiveLiteral, :DirectiveLiteral,
+             :DoWhileStatement, :EmptyStatement, :Eq, :ExceptHandler,
+             :ExportDefaultDeclaration, :ExportNamedDeclaration,
+             :ExportSpecifier, :Expr, :ExpressionStatement, :For,
+             :ForInStatement, :ForStatement, :FunctionDeclaration,
+             :FunctionDef, :FunctionExpression, :Gt, :Identifier, :If,
+             :IfExp, :IfStatement, :Import, :ImportDeclaration,
+             :ImportDefaultSpecifier, :ImportFrom, :ImportSpecifier,
+             :Index, :LabeledStatement, :LogicalExpression, :LtE,
+             :MemberExpression, :Name, :NewExpression, :NotIn,
+             :NullLiteral, :Num, :NumericLiteral, :ObjectExpression,
+             :ObjectMethod, :ObjectPattern, :ObjectProperty, :Or,
+             :Print, :RegExpLiteral, :ReturnStatement,
+             :SequenceExpression, :Slice, :Str, :StringLiteral,
+             :Subscript, :Super, :SwitchCase, :SwitchStatement,
+             :TaggedTemplateExpression, :TemplateElement,
+             :TemplateLiteral, :ThisExpression, :ThrowStatement,
+             :TryExcept, :TryStatement, :Tuple, :UnaryExpression,
+             :UpdateExpression, :VariableDeclaration,
+             :VariableDeclarator, :WhileStatement, :Yield, :alternate,
+             :argument, :arguments, :array_dim_fetch, :assign,
+             :assign_op_minus, :binary_op_bitwise_and,
+             :binary_op_bitwise_or, :binary_op_concat,
+             :binary_op_shift_right, :binary_op_smaller_or_equal,
+             :body, :callee, :cases, :comparators, :consequent,
+             :declaration, :declarations, :directives, :elements,
+             :elts, :exp, :expression, :expressions, :extra,
+             :finalizer, :foreach, :func_call, :function, :id, :init,
+             :init, :key, :keyword, :left, :list, :lnumber, :name,
+             :object, :param, :params, :properties, :property,
+             :quasis, :right, :specifiers, :string, :superClass,
+             :target, :test, :update, :value, :values,
+             :variable
+            ]
 
-  ##
-  # All ruby_parser nodes in an index hash. Used by jenkins algorithm.
-
-  NODE_NAMES = Hash[names.each_with_index.map { |n, i| [n.to_sym, i] }]
-
-  NODE_NAMES.default_proc = lambda { |h, k|
-    $stderr.puts "ERROR: couldn't find node type #{k} in Sexp::NODE_NAMES."
-    h[k] = NODE_NAMES.size
-  }
-
-  MAX_INT32 = 2**32 - 1 # :nodoc:
-
-  def pure_ruby_hash # :nodoc: see above
-    hash = 0
-
-    n = NODE_NAMES.fetch first
-
-    raise "Bad lookup: #{first} in #{sexp.inspect}" unless n
-
-    hash += n & MAX_INT32
-    hash += hash << 10 & MAX_INT32
-    hash ^= hash >> 6 & MAX_INT32
-
-    each do |o|
-      next unless o.is_a? Sexp
-      hash = hash + o.pure_ruby_hash & MAX_INT32
-      hash = (hash + (hash << 10)) & MAX_INT32
-      hash = (hash ^ (hash >> 6)) & MAX_INT32
-    end
-
-    hash = (hash + (hash << 3)) & MAX_INT32
-    hash = (hash ^ (hash >> 11)) & MAX_INT32
-    hash = (hash + (hash << 15)) & MAX_INT32
-
-    hash
-  end
+# Add known javascript and php nodes to the hash registry.
+new_nodes.each do |name|
+  Sexp::NODE_NAMES[name] = Sexp::NODE_NAMES.size
 end
 
 class Sexp
@@ -104,20 +87,6 @@ class Sexp
         end
       else
         r << s
-      end
-    end
-  end
-end
-
-class Sexp # TODO: push this back to flay
-  alias old_mass mass
-
-  def mass
-    @mass ||= inject(1) do |t, s|
-      if s.is_a?(Sexp)
-        t + s.mass
-      else
-        t
       end
     end
   end
