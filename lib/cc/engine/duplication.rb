7d0c2f73d781d72de5aea47501a4f4421921fb6f
Devon Blandin <dblandin@gmail.com>
2017-08-28 17:46:54 -0400

    Add initial Java duplication support
    Adds a new Java language strategy, utilizing the new parser backend.
    
    Kudos and credit to @ABaldwinHunter and @nporteschaikin on the
    `SexpBuilder` implementation!! :+1:

--- lib/cc/engine/duplication.rb
+++ lib/cc/engine/duplication.rb
@@ -2,6 +2,7 @@
 
 require "bundler/setup"
 require "cc/engine/analyzers/ruby/main"
+require "cc/engine/analyzers/java/main"
 require "cc/engine/analyzers/javascript/main"
 require "cc/engine/analyzers/php/main"
 require "cc/engine/analyzers/python/main"
@@ -16,6 +17,7 @@ module CC
     class Duplication
       LANGUAGES = {
         "ruby"       => ::CC::Engine::Analyzers::Ruby::Main,
+        "java"       => ::CC::Engine::Analyzers::Java::Main,
         "javascript" => ::CC::Engine::Analyzers::Javascript::Main,
         "php"        => ::CC::Engine::Analyzers::Php::Main,
         "python"     => ::CC::Engine::Analyzers::Python::Main,
