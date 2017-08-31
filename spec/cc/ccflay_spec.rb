363ecb2dd1139f878b090f0493364b5a2485181e
Todd Mazierski <toddmazierski@users.noreply.github.com>
2017-08-31 16:15:21 -0400

    Add specs to reproduce logging bug (#206)
    Add a spec to reproduce `couldn't find node type` errors being logged.
    It fails with:
    
        expected block to not output to stderr, but output "ERROR: couldn't
        find node type bug_206_node in Sexp::NODE_NAMES.\n"
    
    Add a spec to regression test the automatic assignment of incrementing
    values for missing nodes.

--- spec/cc/ccflay_spec.rb
+++ spec/cc/ccflay_spec.rb
@@ -20,4 +20,22 @@ RSpec.describe CCFlay do
       expect(inn.flatter.mass).to eq(8)
     end
   end
+
+  describe Sexp::NODE_NAMES do
+    describe ".default_proc" do
+      it "should automatically assign incrementing values for missing nodes" do
+        node1 = Sexp::NODE_NAMES["some_missing_node1"]
+        node2 = Sexp::NODE_NAMES["some_missing_node2"]
+
+        expect(node1).to be > 0
+        expect(node2).to eq(node1 + 1)
+      end
+
+      context "'couldn't find node type' errors (bug #206)" do
+        it "should suppress them" do
+          expect { Sexp::NODE_NAMES["bug_206_node"] }.to_not output.to_stderr
+        end
+      end
+    end
+  end
 end
