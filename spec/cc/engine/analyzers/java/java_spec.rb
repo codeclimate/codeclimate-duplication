a06cc25f4e5ae755ce0b420ea2aade9325ecf795
Max Jacobson <max@hardscrabble.net>
2017-09-25 14:30:20 -0400

    Reproduce bug

--- spec/cc/engine/analyzers/java/java_spec.rb
+++ spec/cc/engine/analyzers/java/java_spec.rb
@@ -4,7 +4,7 @@ require "cc/engine/analyzers/engine_config"
 require "cc/engine/analyzers/file_list"
 
 module CC::Engine::Analyzers
-  RSpec.describe Java::Main, in_tmpdir: true do
+  RSpec.describe Java::Main, in_tmpdir: true, focus: true do
     include AnalyzerSpecHelpers
 
     describe "#run" do
@@ -141,6 +141,75 @@ import org.springframework.rules.closure.BinaryConstraint;
 
         issues = run_engine(engine_conf).strip.split("\0")
         expect(issues).to be_empty
+      end
+
+      it "prints an issue for similar code when the only difference is the value of two hexadecimal numbers" do
+        create_source_file("foo.java", <<-EOF)
+          public class ArrayDemo {
+            public static void foo() {
+              int[] scott;
+              scott = new int[] {
+                0x00000000, 0x77073096, 0xEE0E612C, 0x990951BA, 0x076DC419, 0x706AF48F
+              };
+
+              int[] anArray;
+
+              anArray = new int[10];
+
+              for (int i = 0; i < anArray.length; i++) {
+                anArray[i] = i;
+              }
+
+              for (int i = 0; i < anArray.length; i++) {
+                System.out.print(anArray[i] + " ");
+              }
+
+              System.out.println();
+            }
+
+            public static void foo() {
+              int[] scott;
+              scott = new int[] {
+                0xF3B97148, 0x84BE41DE, 0x1ADAD47D, 0x6DDDE4EB, 0xF4D4B551, 0x83D385C7
+              };
+
+              int[] anArray;
+
+              anArray = new int[10];
+
+              for (int i = 0; i < anArray.length; i++) {
+                anArray[i] = i;
+              }
+
+              for (int i = 0; i < anArray.length; i++) {
+                System.out.print(anArray[i] + " ");
+              }
+
+              System.out.println();
+            }
+          }
+        EOF
+
+        issues = run_engine(engine_conf).strip.split("\0")
+        expect(issues.length).to be > 0
+        result = issues.first.strip
+        json = JSON.parse(result)
+
+        expect(json["type"]).to eq("issue")
+        expect(json["check_name"]).to eq("similar-code")
+        # expect(json["description"]).to eq("Similar blocks of code found in 2 locations. Consider refactoring.")
+        # expect(json["categories"]).to eq(["Duplication"])
+        # expect(json["location"]).to eq({
+        #   "path" => "foo.java",
+        #   "lines" => { "begin" => 2, "end" => 16 },
+        # })
+        # expect(json["remediation_points"]).to eq(930_000)
+        # expect(json["other_locations"]).to eq([
+        #   {"path" => "foo.java", "lines" => { "begin" => 18, "end" => 32 } },
+        # ])
+        # expect(json["content"]["body"]).to match /This issue has a mass of 103/
+        # expect(json["fingerprint"]).to eq("48eb151dc29634f90a86ffabf9d3c4b5")
+        # expect(json["severity"]).to eq(CC::Engine::Analyzers::Base::MAJOR)
       end
     end
   end
