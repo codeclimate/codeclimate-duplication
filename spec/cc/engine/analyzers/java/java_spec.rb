9f6023719d673af5934f591aa7d7cdaf35623aee
Max Jacobson <max@hardscrabble.net>
2017-09-25 17:45:38 -0400

    Bump cc-parser to b378
    Pulls in https://github.com/codeclimate/codeclimate-parser/pull/89
    
    Paired with @larkinscott on this

--- spec/cc/engine/analyzers/java/java_spec.rb
+++ spec/cc/engine/analyzers/java/java_spec.rb
@@ -142,6 +142,76 @@ import org.springframework.rules.closure.BinaryConstraint;
         issues = run_engine(engine_conf).strip.split("\0")
         expect(issues).to be_empty
       end
+
+      it "prints an issue for similar code when the only difference is the value of a literal" do
+        create_source_file("foo.java", <<-EOF)
+          public class ArrayDemo {
+            public static void foo() {
+              int[] scott;
+              scott = new int[] {
+                0x00000000, 0x77073096, 0xEE0E612C, 0x990951BA, 0x076DC419, 0x706AF48F
+              };
+
+              int[] anArray;
+
+              anArray = new int[10];
+
+              for (int i = 0; i < anArray.length; i++) {
+                anArray[i] = i;
+              }
+
+              for (int i = 0; i < anArray.length; i++) {
+                System.out.print(anArray[i] + " ");
+              }
+
+              System.out.println();
+            }
+
+            public static void foo() {
+              int[] scott;
+              scott = new int[] {
+                0xF3B97148, 0x84BE41DE, 0x1ADAD47D, 0x6DDDE4EB, 0xF4D4B551, 0x83D385C7
+              };
+
+              int[] anArray;
+
+              anArray = new int[10];
+
+              for (int i = 0; i < anArray.length; i++) {
+                anArray[i] = i;
+              }
+
+              for (int i = 0; i < anArray.length; i++) {
+                System.out.print(anArray[i] + " ");
+              }
+
+              System.out.println();
+            }
+          }
+        EOF
+
+        issues = run_engine(engine_conf).strip.split("\0")
+        expect(issues.length).to be > 0
+        result = issues.first.strip
+        json = JSON.parse(result)
+
+        expect(json["type"]).to eq("issue")
+        expect(json["check_name"]).to eq("similar-code")
+
+        expect(json["description"]).to eq("Similar blocks of code found in 2 locations. Consider refactoring.")
+        expect(json["categories"]).to eq(["Duplication"])
+        expect(json["location"]).to eq({
+          "path" => "foo.java",
+          "lines" => { "begin" => 2, "end" => 21 },
+        })
+        expect(json["remediation_points"]).to eq(1_230_000)
+        expect(json["other_locations"]).to eq([
+          {"path" => "foo.java", "lines" => { "begin" => 23, "end" => 42 } },
+        ])
+        expect(json["content"]["body"]).to match /This issue has a mass of 133/
+        expect(json["fingerprint"]).to eq("9abf88bac3a56bf708a5c4ceaf251d98")
+        expect(json["severity"]).to eq(CC::Engine::Analyzers::Base::MAJOR)
+      end
     end
   end
 end
