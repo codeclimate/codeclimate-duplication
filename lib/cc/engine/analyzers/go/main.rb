8ea6ef38d18f1416bdd4f374f4d64224e88254d6
Scott Larkin <scott@codeclimate.com>
2017-12-07 14:52:46 -0500

    Add Go duplication checks to channel/go-beta (#288) (#293)
    * Add main.rb for Go duplication
    
    * Add spec
    
    * Add Go to duplication
    
    * Add default_filters for imports/comments
    
    * Tune mass threshold
    
    * Remove unneeded default_filters, update comment filtering
    
    * Fix indentation nit

---
+++ lib/cc/engine/analyzers/go/main.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "flay"
+require "json"
+require "cc/engine/analyzers/reporter"
+require "cc/engine/analyzers/analyzer_base"
+
+module CC
+  module Engine
+    module Analyzers
+      module Go
+        class Main < CC::Engine::Analyzers::Base
+          LANGUAGE = "go"
+          PATTERNS = ["**/*.go"].freeze
+          DEFAULT_MASS_THRESHOLD = 30
+          DEFAULT_FILTERS = [
+            "(ImportSpec ___)",
+          ].freeze
+          POINTS_PER_OVERAGE = 40_000
+          REQUEST_PATH = "/go"
+          COMMENT_MATCHER = Sexp::Matcher.parse("(_ (comments ___) ___)")
+
+          def use_sexp_lines?
+            false
+          end
+
+          def transform_sexp(sexp)
+            delete_comments!(sexp)
+          end
+
+          private
+
+          def process_file(file)
+            parse(file, REQUEST_PATH)
+          end
+
+          def default_filters
+            DEFAULT_FILTERS.map { |filter| Sexp::Matcher.parse filter }
+          end
+
+          def delete_comments!(sexp)
+            sexp.search_each(COMMENT_MATCHER) { |node| node.delete_at(1) }
+          end
+        end
+      end
+    end
+  end
+end
