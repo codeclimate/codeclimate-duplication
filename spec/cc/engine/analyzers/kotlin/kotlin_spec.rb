a62c8222b5d6c68f4cef4d1d645de539a19b4791
Hersh Amin <h.amin@utexas.edu>
2018-06-05 00:04:47 -0500

    [wip] Added kotlin duplication checks (2)

--- spec/cc/engine/analyzers/kotlin/kotlin_spec.rb
+++ spec/cc/engine/analyzers/kotlin/kotlin_spec.rb
@@ -1,48 +1,43 @@
-=begin
 require "spec_helper"
-require "cc/engine/analyzers/java/main"
+require "cc/engine/analyzers/kotlin/main"
 require "cc/engine/analyzers/engine_config"
 
 module CC::Engine::Analyzers
-  RSpec.describe Java::Main, in_tmpdir: true do
+  RSpec.describe Kotlin::Main, in_tmpdir: true do
     include AnalyzerSpecHelpers
 
     describe "#run" do
       let(:engine_conf) { EngineConfig.new({}) }
 
       it "prints an issue for similar code" do
-        create_source_file("foo.java", <<-EOF)
-          public class ArrayDemo {
-            public static void foo() {
-              int[] anArray;
+        create_source_file("foo.kt", <<-EOF)
+          class ArrayDemo {
+            fun foo() {
+              val anArray: Array<Int> = Array(10)
 
-              anArray = new int[10];
-
-              for (int i = 0; i < anArray.length; i++) {
-                anArray[i] = i;
+              for (i in 0..10) {
+                anArray[i] = i
               }
 
-              for (int i = 0; i < anArray.length; i++) {
-                System.out.print(anArray[i] + " ");
+              for (i in 0..10) {
+                println(anArray[i])
               }
 
-              System.out.println();
+              println("")
             }
 
-            public static void bar() {
-              int[] anArray;
+            fun bar() {
+              val anArray: Array<Int> = Array(10)
 
-              anArray = new int[10];
-
-              for (int i = 0; i < anArray.length; i++) {
-                anArray[i] = i;
+              for (i in 0..10) {
+                anArray[i] = i
               }
 
-              for (int i = 0; i < anArray.length; i++) {
-                System.out.print(anArray[i] + " ");
+              for (i in 0..10) {
+                println(anArray[i])
               }
 
-              System.out.println();
+              println("")
             }
           }
         EOF
@@ -56,35 +51,32 @@ module CC::Engine::Analyzers
         expect(json["description"]).to eq("Similar blocks of code found in 2 locations. Consider refactoring.")
         expect(json["categories"]).to eq(["Duplication"])
         expect(json["location"]).to eq({
-          "path" => "foo.java",
-          "lines" => { "begin" => 2, "end" => 16 },
+          "path" => "foo.kt",
+          "lines" => { "begin" => 2, "end" => 14 },
         })
-        expect(json["remediation_points"]).to eq(930_000)
         expect(json["other_locations"]).to eq([
-          {"path" => "foo.java", "lines" => { "begin" => 18, "end" => 32 } },
+          {"path" => "foo.kt", "lines" => { "begin" => 16, "end" => 28 } },
         ])
-        expect(json["content"]["body"]).to match /This issue has a mass of 103/
-        expect(json["fingerprint"]).to eq("48eb151dc29634f90a86ffabf9d3c4b5")
         expect(json["severity"]).to eq(CC::Engine::Analyzers::Base::MAJOR)
       end
 
       it "prints an issue for identical code" do
-        create_source_file("foo.java", <<-EOF)
-          public class ArrayDemo {
-            public static void foo(int[] anArray) {
-              for (int i = 0; i < anArray.length; i++) {
-                System.out.print(anArray[i] + " ");
+        create_source_file("foo.kt", <<-EOF)
+          class ArrayDemo {
+            fun foo(anArray: Array<Int>) {
+              for (i in anArray.indices) {
+                println(anArray[i] + " ")
               }
 
-              System.out.println();
+              println("")
             }
 
-            public static void foo(int[] anArray) {
-              for (int i = 0; i < anArray.length; i++) {
-                System.out.print(anArray[i] + " ");
+            fun foo(anArray: Array<Int>) {
+              for (i in anArray.indices) {
+                println(anArray[i] + " ")
               }
 
-              System.out.println();
+              println("")
             }
           }
         EOF
@@ -98,20 +90,17 @@ module CC::Engine::Analyzers
         expect(json["description"]).to eq("Identical blocks of code found in 2 locations. Consider refactoring.")
         expect(json["categories"]).to eq(["Duplication"])
         expect(json["location"]).to eq({
-          "path" => "foo.java",
+          "path" => "foo.kt",
           "lines" => { "begin" => 2, "end" => 8 },
         })
-        expect(json["remediation_points"]).to eq(420_000)
         expect(json["other_locations"]).to eq([
-          {"path" => "foo.java", "lines" => { "begin" => 10, "end" => 16 } },
+          {"path" => "foo.kt", "lines" => { "begin" => 10, "end" => 16 } },
         ])
-        expect(json["content"]["body"]).to match /This issue has a mass of 52/
-        expect(json["fingerprint"]).to eq("dbb957b34f7b5312538235c0aa3f52a0")
-        expect(json["severity"]).to eq(CC::Engine::Analyzers::Base::MINOR)
+        expect(json["severity"]).to eq(CC::Engine::Analyzers::Base::MAJOR)
       end
 
       it "outputs a warning for unprocessable errors" do
-        create_source_file("foo.java", <<-EOF)
+        create_source_file("foo.kt", <<-EOF)
           ---
         EOF
 
@@ -121,22 +110,22 @@ module CC::Engine::Analyzers
       end
 
       it "ignores import and package declarations" do
-        create_source_file("foo.java", <<-EOF)
-package org.springframework.rules.constraint;
+        create_source_file("foo.kt", <<-EOF)
+          package org.springframework.rules.constraint;
 
-import java.util.Comparator;
+          import java.util.Comparator;
 
-import org.springframework.rules.constraint.Constraint;
-import org.springframework.rules.closure.BinaryConstraint;
+          import org.springframework.rules.constraint.Constraint;
+          import org.springframework.rules.closure.BinaryConstraint;
         EOF
 
-        create_source_file("bar.java", <<-EOF)
-package org.springframework.rules.constraint;
+        create_source_file("bar.kt", <<-EOF)
+          package org.springframework.rules.constraint;
 
-import java.util.Comparator;
+          import java.util.Comparator;
 
-import org.springframework.rules.constraint.Constraint;
-import org.springframework.rules.closure.BinaryConstraint;
+          import org.springframework.rules.constraint.Constraint;
+          import org.springframework.rules.closure.BinaryConstraint;
         EOF
 
         issues = run_engine(engine_conf).strip.split("\0")
@@ -144,48 +133,42 @@ import org.springframework.rules.closure.BinaryConstraint;
       end
 
       it "prints an issue for similar code when the only difference is the value of a literal" do
-        create_source_file("foo.java", <<-EOF)
-          public class ArrayDemo {
-            public static void foo() {
-              int[] scott;
-              scott = new int[] {
+        create_source_file("foo.kt", <<-EOF)
+          class ArrayDemo {
+            fun foo() {
+              val scott = arrayOfInt(
                 0x00000000, 0x77073096, 0xEE0E612C, 0x990951BA, 0x076DC419, 0x706AF48F
-              };
+              )
 
-              int[] anArray;
+              val anArray: Array<Int> = Array(10)
 
-              anArray = new int[10];
-
-              for (int i = 0; i < anArray.length; i++) {
-                anArray[i] = i;
+              for (i in 0..<10) {
+                anArray[i] = i
               }
 
-              for (int i = 0; i < anArray.length; i++) {
-                System.out.print(anArray[i] + " ");
+              for (i in 0..<10) {
+                println(anArray[i] + " ")
               }
 
-              System.out.println();
+              println()
             }
 
-            public static void foo() {
-              int[] scott;
-              scott = new int[] {
+            fun foo() {
+              val scott = arrayOfInt(
                 0xF3B97148, 0x84BE41DE, 0x1ADAD47D, 0x6DDDE4EB, 0xF4D4B551, 0x83D385C7
-              };
+              )
 
-              int[] anArray;
+              val anArray: Array<Int> = Array(10)
 
-              anArray = new int[10];
-
-              for (int i = 0; i < anArray.length; i++) {
-                anArray[i] = i;
+              for (i in 0..<10) {
+                anArray[i] = i
               }
 
-              for (int i = 0; i < anArray.length; i++) {
-                System.out.print(anArray[i] + " ");
+              for (i in 0..<10) {
+                println(anArray[i] + " ")
               }
 
-              System.out.println();
+              println()
             }
           }
         EOF
@@ -201,18 +184,15 @@ import org.springframework.rules.closure.BinaryConstraint;
         expect(json["description"]).to eq("Similar blocks of code found in 2 locations. Consider refactoring.")
         expect(json["categories"]).to eq(["Duplication"])
         expect(json["location"]).to eq({
-          "path" => "foo.java",
-          "lines" => { "begin" => 2, "end" => 21 },
+          "path" => "foo.kt",
+          "lines" => { "begin" => 2, "end" => 18 },
         })
-        expect(json["remediation_points"]).to eq(1_230_000)
         expect(json["other_locations"]).to eq([
-          {"path" => "foo.java", "lines" => { "begin" => 23, "end" => 42 } },
+          {"path" => "foo.kt", "lines" => { "begin" => 20, "end" => 36 } },
         ])
-        expect(json["content"]["body"]).to match /This issue has a mass of 133/
-        expect(json["fingerprint"]).to eq("9abf88bac3a56bf708a5c4ceaf251d98")
         expect(json["severity"]).to eq(CC::Engine::Analyzers::Base::MAJOR)
       end
+
     end
   end
 end
-=end
