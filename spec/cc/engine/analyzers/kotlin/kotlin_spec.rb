dda1a2203e73514d1abfe47972e280b9f6a6bcc0
Scott Larkin <scott@codeclimate.com>
2018-06-12 16:43:34 -0400

    Shorten the example comments to a few lines

--- spec/cc/engine/analyzers/kotlin/kotlin_spec.rb
+++ spec/cc/engine/analyzers/kotlin/kotlin_spec.rb
@@ -195,25 +195,9 @@ module CC::Engine::Analyzers
 
       it "ignores comment docs and comments" do
         create_source_file("foo.kt", <<-EOF)
-           /*******************************************************************************
-            *                                                                             *
-            *  Copyright (C) 2017 by Max Lv <max.c.lv@gmail.com>                          *
-            *  Copyright (C) 2017 by Mygod Studio <contact-shadowsocks-android@mygod.be>  *
-            *                                                                             *
-            *  This program is free software: you can redistribute it and/or modify       *
-            *  it under the terms of the GNU General Public License as published by       *
-            *  the Free Software Foundation, either version 3 of the License, or          *
-            *  (at your option) any later version.                                        *
-            *                                                                             *
-            *  This program is distributed in the hope that it will be useful,            *
-            *  but WITHOUT ANY WARRANTY; without even the implied warranty of             *
-            *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
-            *  GNU General Public License for more details.                               *
-            *                                                                             *
-            *  You should have received a copy of the GNU General Public License          *
-            *  along with this program. If not, see <http://www.gnu.org/licenses/>.       *
-            *                                                                             *
-            *******************************************************************************/
+           /********************************************************************
+            *  Copyright (C) 2017 by Max Lv <max.c.lv@gmail.com>
+            *******************************************************************/
 
            package com.github.shadowsocks.acl
            // Comment here
@@ -239,25 +223,9 @@ module CC::Engine::Analyzers
         EOF
 
         create_source_file("bar.kt", <<-EOF)
-          /*******************************************************************************
-           *                                                                             *
-           *  Copyright (C) 2017 by Max Lv <max.c.lv@gmail.com>                          *
-           *  Copyright (C) 2017 by Mygod Studio <contact-shadowsocks-android@mygod.be>  *
-           *                                                                             *
-           *  This program is free software: you can redistribute it and/or modify       *
-           *  it under the terms of the GNU General Public License as published by       *
-           *  the Free Software Foundation, either version 3 of the License, or          *
-           *  (at your option) any later version.                                        *
-           *                                                                             *
-           *  This program is distributed in the hope that it will be useful,            *
-           *  but WITHOUT ANY WARRANTY; without even the implied warranty of             *
-           *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
-           *  GNU General Public License for more details.                               *
-           *                                                                             *
-           *  You should have received a copy of the GNU General Public License          *
-           *  along with this program. If not, see <http://www.gnu.org/licenses/>.       *
-           *                                                                             *
-           *******************************************************************************/
+          /*********************************************************************
+           *  Copyright (C) 2017 by Max Lv <max.c.lv@gmail.com>
+           ********************************************************************/
 
           package com.evernote.android.job
           // Comment here
