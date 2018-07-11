0c23d20d7ed0cc1e4af340c21c80ba9fb234d020
Steven R. Baker <steven@stevenrbaker.com>
2018-07-11 19:15:54 +0200

    Add C# duplication checks.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM codeclimate/codeclimate-parser:b764
+FROM codeclimate/codeclimate-parser:latest
 LABEL maintainer="Code Climate <hello@codeclimate.com>"
 
 # Reset from base image
---
+++ lib/cc/engine/analyzers/csharp/main.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "flay"
+require "json"
+require "cc/engine/analyzers/reporter"
+require "cc/engine/analyzers/analyzer_base"
+
+module CC
+  module Engine
+    module Analyzers
+      module Csharp
+        class Main < CC::Engine::Analyzers::Base
+          LANGUAGE = "csharp".freeze
+          PATTERNS = ["**/*.cs"].freeze
+          DEFAULT_MASS_THRESHOLD = 60
+          DEFAULT_FILTERS = [
+            "(UsingDirective ___)".freeze
+          ].freeze
+          POINTS_PER_OVERAGE = 10_000
+          REQUEST_PATH = "/csharp".freeze
+
+          def use_sexp_lines?
+            false
+          end
+
+          private
+
+          def process_file(file)
+            parse(file, REQUEST_PATH)
+          end
+
+          def default_filters
+            DEFAULT_FILTERS.map { |filter| Sexp::Matcher.parse filter }
+          end
+        end
+      end
+    end
+  end
+end
